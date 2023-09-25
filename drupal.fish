17c7dafe2ec239584dbfbcfcba099f50b68d6144
Jon Pugh <jon@thinkdrop.net>
2023-09-25 14:16:17 -0400

    Add fish config.

---
+++ drupal.fish
@@ -0,0 +1,24 @@
+#
+# Copy this file to ~/.config/fish/conf.d/drupal.fish
+#
+
+# Fish abbreviations.
+abbr --add c composer
+abbr --add dd ddev
+abbr --add ddd ddev drush
+abbr --add d docker
+abbr --add dc docker-compose
+abbr --add l lando
+abbr --add lc lando composer
+abbr --add ld lando drush
+abbr --add g git
+abbr --add gb git branch
+abbr --add gc git commit
+abbr --add gd git diff
+abbr --add gs git status
+abbr --add gt git tag
+abbr --add gco git checkout
+abbr --add gcp git cherry-pick
+abbr --add gp git push
+abbr --add gpl git pull
+
